244e3de5f5765fd89402c547bc45fec20ff3c09a
AYESDIE <34449856+AYESDIE@users.noreply.github.com>
2018-10-09 19:05:26 +0530

    Update a random node problem/main.cpp

--- a random node problem/main.cpp
+++ a random node problem/main.cpp
@@ -36,44 +36,37 @@ std::vector<coordinate> makecoordinate(int edgenode,int length=4){
     return temp;
 }
 
-//void fspt(int n){
-//    std::vector<coordinate> c = makecoordinate(n);
-//    foo boo(c);
-//    double x,y;
-//    long long int step,ac=0,wa=0;
-//    int length = 4;
-//
-//    std::cout<<">> x : ";
-//    std::cin>>x;
-//    std::cout<<">> y : ";
-//    std::cin>>y;
-//    std::cout<<">> step size : ";
-//    std::cin>>step;
-//
-//    for (int i = 0; i <= length*step; i++) {
-//        for (int j = 0; j <= length*step; ++j) {
-//            double a = (double)i/step;
-//            double b = (double)j/step;
-//            coordinate cx(a,b);
-//            n = boo.evaluate(cx);
-//            if(n==2){
-//                ac++;
-//            }
-//            else{
-//                wa++;
-//            }
-//        }
-//    }
-//
-//    std::cout<<"\n>> AC : "<<ac<<"\n>> WA : "<<wa;
-//}
+void fspt(foo boo,int l){
+    long long int step,amp;
+    long long int ac=0,wa=0;
+    coordinate p(0,0);
+    std::cout<<"\nStarting coordinate:"<<p;
+    std::cout<<"\n>> Step Size : ";
+    std::cin>>step;
 
-void arv(int n){
-    std::vector<coordinate> c = makecoordinate(n);
+    long long int length = step*l;
+    amp = l;
+    for (long long int i = 0; i <= length ; i+=amp) {
+        for (long long int j = 0; j <= length ; j+=amp) {
+            double a,b;
+            a = (double)i / step;
+            b = (double)j / step;
+            coordinate cx(a,b);
+            int n = boo.evaluate(cx);
+            if(n==2){
+                ac++;
+            }
+            else{
+                wa++;
+            }
+        }
+    }
+    std::cout<<"\n>> AC : "<<ac<<"\n>> WA : "<<wa;
+}
+
+void arv(foo boo){
     long long int t,ac=0,wa=0;
-    foo boo(c);
     std::default_random_engine generator;
-
     std::cout<<"\n Number of iterations : ";
     std::cin>>t;
     std::uniform_real_distribution<double> distribution(0,4);
@@ -91,27 +84,27 @@ void arv(int n){
     std::cout<<"\n>> AC : "<<ac<<"\n>> WA : "<<wa;
 }
 
-//void rspt(int n){
-//    std::vector<coordinate> c = makecoordinate(n);
-//    foo boo(c);
-//    double x,y;
-//    long long int step,ac=0,wa=0;
-//    int length = 4;
-//
+//void rspt(foo boo, int l){
+//    long long int step,amp;
+//    long long int ac=0,wa=0;
 //    std::default_random_engine generator;
-//    std::uniform_real_distribution<double> distribution(0,1);
+//    std::uniform_real_distribution<double> distribution(0,l);
 //    double roll1 = distribution(generator);
-//    double roll2 = distribution(generator);
-//
-//    std::cout<<">> step size : ";
+//    roll1=0;
+//    coordinate p(roll1,0);
+//    std::cout<<"\nStarting coordinate:"<<p;
+//    std::cout<<"\n>> step size : ";
 //    std::cin>>step;
 //
-//    for (int i = 0; i <= length*step; i++) {
-//        for (int j = 0; j <= length*step; ++j) {
-//            double a = (double)i/step;
-//            double b = (double)j/step;
+//    long long int length = step*l;
+//    amp = l;
+//    for (long long int i = 0; i <= length ; i+=amp) {
+//        for (long long int j = 0; j <= length ; j+=amp) {
+//            double a,b;
+//            a = (double)i / step;
+//            b = (double)j / step;
 //            coordinate cx(a,b);
-//            n = boo.evaluate(cx);
+//            int n = boo.evaluate(cx);
 //            if(n==2){
 //                ac++;
 //            }
@@ -122,16 +115,14 @@ void arv(int n){
 //    }
 //
 //    std::cout<<"\n>> AC : "<<ac<<"\n>> WA : "<<wa;
-//
 //}
 
-void mc(int n){
-    std::vector<coordinate> c = makecoordinate(n);
-    foo boo(c);
+void mc(foo boo){
     long long int t;
+    int n;
     std::cout<<"\n Number of iterations : ";
     std::cin>>t;
-    for (int i = 0; i < 10; ++i) {
+    for (int i = 0; i < t; ++i) {
         double l,q;
         std::cout<<"\n>> x: ";
         std::cin>>l;
@@ -147,92 +138,41 @@ void mc(int n){
         }
         std::cout<<"\n-----------------------------------------------\n";
     }
-
-
-
 }
 
 int main(){
 
     int a=0,b=0,p=0,n;
-    int c;
+    int c,l=4;
     std::cout<<"\n================================================\n";
     std::cout<<" Enter nodes per side(minimum = 2): ";
     std::cin>>n;
+    std::cout<<" Enter length of a side(default = 4): ";
+    std::cin>>l;
     n=n-2;
-    std::vector<coordinate> x = makecoordinate(n);
+    std::vector<coordinate> x = makecoordinate(n,l);
     foo boo(x);
-//    for (int i = 0; i < x.size(); ++i) {
-//        std::cout<<"\n>> "<<x[i];
-//    }
-    std::cout<<"\n 1) All random values\n 2) Fixed starting point and traversal\n 3) Random starting point and traversal \n 4) Manual Checking\n >> ";
+    std::cout<<"\nAll the coordinates:\n";
+    for (int i = 0; i < x.size(); ++i) {
+        std::cout<<"\n>> "<<x[i];
+    }
+    std::cout<<"\n 1) All random values\n 2) Fixed starting point and traversal\n 3) Fixed starting point with noise and traversal \n 4) Manual Checking\n >> ";
     std::cin>>c;
     switch (c){
         case 1:
-            arv(n);
+            arv(boo);
             break;
         case 2:
-            //fspt(n);
+            fspt(boo,l);
             break;
         case 3:
-            //rspt(n);
+            //rspt(boo,l);
             break;
         case 4:
-            mc(n);
+            mc(boo);
             break;
         default:
             std::cout<<"\n>>  Consider Rechecking";
     }
-
-    std::default_random_engine generator;
-    std::uniform_real_distribution<double> distribution(0,4);
-//    for (int i = 0; i < 100000; ++i) {
-//        double roll1 = distribution(generator);
-//        double roll2 = distribution(generator);
-//        coordinate cx(roll1,roll2);
-//        if(boo.evaluate(cx)==1){
-//            std::cout<<cx;
-//            std::cout<<" >>"<<boo.evaluate(cx)<<"\n";
-//        }
-//    }
-
-//    int step = 100;
-//    int length = 4;
-//    for (int i = 0; i <= length*step; i++) {
-//        for (int j = 0; j <= length*step; ++j) {
-//            std::cout<<i<<" "<<j<<"\n";
-//            double x = (double)i/step;
-//            double y = (double)j/step;
-//            coordinate cx(x,y);
-//            n = boo.evaluate(cx);
-//            if(n==2){
-//                a++;
-//            }
-//            else if(n==1){
-//                b++;
-//            }
-//            else{
-//                p++;
-//            }
-//        }
-//    }
-//
-//    std::cout<<a<<"\n"<<b<<"\n"<<p;
-
-//    for (int i = 0; i < 10; ++i) {
-//        double l,q;
-//        std::cin>> l>>q;
-//        coordinate cx(l,q);
-//        n = boo.evaluate(cx);
-//            if(n==2){
-//                std::cout<<"AC";
-//            }
-//            else if(n==1){
-//                std::cout<<"PC";
-//            }
-//            else{
-//                std::cout<<"WA";
-//            }
-//    }
-
+    return 0;
 }
